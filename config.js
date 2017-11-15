e3aa508b8ab0faa36cc2ffc1ca7e91cd8959d0b3
Andy Chen <shinningstar1001@gmail.com>
2017-11-15 19:04:55 +0800

    ADD all testing url

--- config.js
+++ config.js
@@ -1,33 +1,48 @@
 module.exports = {
     tasks: [{
-        url: 'https://www.188bet.co.uk/en-gb/',
+        url: 'https://www.188bet.co.uk/en-gb',
         harFolder: 'dist',
         jobInterval: 15,
         enable: true
     }, {
         url: 'https://www.188bet.co.uk/en-gb/sports',
         harFolder: 'dist',
+        jobInterval: 60,
+        enable: true
+    }, {
+        url: 'https://m.188bet.co.uk/en-gb',
+        harFolder: 'dist',
         jobInterval: 15,
         enable: true
     }, {
-        url: 'https://m.188bet.co.uk/en-gb/',
+        url: 'https://www.ez-188uk.com/en-gb',
         harFolder: 'dist',
         jobInterval: 15,
-        enable: false
-    }, {
-        url: 'https://m.188uk.com/en-gb/',
-        harFolder: 'dist',
-        jobInterval: 15,
-        enable: false
-    }, {
-        url: 'https://www.ez-188uk.com/en-gb/',
-        harFolder: 'dist',
-        jobInterval: 15,
-        enable: false
+        enable: true
     }, {
         url: 'https://www.ez-188uk.com/en-gb/sports',
         harFolder: 'dist',
+        jobInterval: 60,
+        enable: true
+    }, {
+        url: 'https://m.ez-188uk.com/en-gb',
+        harFolder: 'dist',
         jobInterval: 15,
-        enable: false
-    },]
+        enable: true
+    }, {
+        url: 'https://nogtm-www.ez-188uk.com/en-gb',
+        harFolder: 'dist',
+        jobInterval: 15,
+        enable: true
+    }, {
+        url: 'https://nogtm-www.ez-188uk.com/en-gb/sports',
+        harFolder: 'dist',
+        jobInterval: 60,
+        enable: true
+    }, {
+        url: 'https://nogtm-m.ez-188uk.com/en-gb',
+        harFolder: 'dist',
+        jobInterval: 15,
+        enable: true
+    }]
 }
