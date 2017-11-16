618cf23d3d6f4f9a94210dae3c4a48bb2e37fa36
Andy Chen <shinningstar1001@gmail.com>
2017-11-16 16:19:16 +0800

    update cleanup path

--- clean.js
+++ clean.js
@@ -1,3 +1,5 @@
 const { cleanup } = require('har-generator');
+const path = require("path");
+const chromePath = path.join(__dirname, "local-chromium\\win64-515411\\chrome-win32\\chrome.exe");
 
-cleanup({ chromePath: "local-chromium\\win64-515411\\chrome-win32\\chrome.exe" })
+cleanup({ chromePath })
