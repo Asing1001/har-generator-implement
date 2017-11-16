f7d8a857c1a8255d4758c1f227a40d3e4f1cd8c6
Andy Chen <shinningstar1001@gmail.com>
2017-11-16 13:37:20 +0800

    add nssm script

--- service.js
+++ service.js
@@ -3,8 +3,9 @@ const path = require("path");
 
 const startFilePath = path.join(__dirname, "start.bat");
 const cmd = `SchTasks /Create /SC DAILY /TN \"har-generator\" /TR \"${startFilePath}\" /ST 09:00`;
-
-exec(cmd, (err, stdout, stderr) => {
+const nssm = `nssm install \"agilehar\" \"${startFilePath}\"`;
+ 
+exec(nssm, (err, stdout, stderr) => {
     if (err)
         console.error('reInitChrome throw error', err);
 
