4f797bc50fe0b66421c1d382b43448a0a061913f
Andy Chen <shinningstar1001@gmail.com>
2017-11-16 13:04:01 +0800

    add nssm and service

---
+++ service.js
@@ -0,0 +1,16 @@
+const { exec } = require("child_process");
+const path = require("path");
+
+const startFilePath = path.join(__dirname, "start.bat");
+const cmd = `SchTasks /Create /SC DAILY /TN \"har-generator\" /TR \"${startFilePath}\" /ST 09:00`;
+
+exec(cmd, (err, stdout, stderr) => {
+    if (err)
+        console.error('reInitChrome throw error', err);
+
+    if (stderr)
+        console.error('stderr:', stderr);
+
+    if (stdout)
+        console.info('stdout:', stdout);
+})
