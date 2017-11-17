d54dc499d587aa77d8687a5ee726a42c2b9e1c13
AndyChen <shinningstar1001@gmail.com>
2017-11-17 16:43:48 +0800

    change config and use new package

--- config.js
+++ config.js
@@ -2,39 +2,39 @@ module.exports = {
     outputFolder: 'C:\\Users\\ytops01\\OneDrive - Global ICT',
     tasks: [{
         url: 'https://www.188bet.co.uk/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }, {
         url: 'https://www.188bet.co.uk/en-gb/sports',
-        jobInterval: 60,
+        jobInterval: 15,
         enable: true
     }, {
         url: 'https://m.188bet.co.uk/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }, {
         url: 'https://www.ez-188uk.com/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }, {
         url: 'https://www.ez-188uk.com/en-gb/sports',
-        jobInterval: 60,
+        jobInterval: 15,
         enable: true
     }, {
         url: 'https://m.ez-188uk.com/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }, {
         url: 'https://nogtm-www.ez-188uk.com/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }, {
         url: 'https://nogtm-www.ez-188uk.com/en-gb/sports',
-        jobInterval: 60,
+        jobInterval: 15,
         enable: true
     }, {
         url: 'https://nogtm-m.ez-188uk.com/en-gb',
-        jobInterval: 5,
+        jobInterval: 3,
         enable: true
     }]
 }
